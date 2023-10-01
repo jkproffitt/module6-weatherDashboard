9a0e6ef3158b4e548b665608cf6568d2956f76f1
jproff <jessicakproffitt@gmail.com>
2023-09-30 20:05:55 -0400

    saving the cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,88 @@
 var apiKey = '8ddd94af0062c9e0bd0ba08b3a7a2ee5';
-var city = '';
-function search() {
-	city = document.getElementById('search-input').value;
-	console.log(city);
-	callFetch(city);
+var searchFormEl = document.querySelector('#search-form');
+var searchBtn = document.querySelector('#search');
+var cityInput = document.querySelector('#search-input');
+var resultText = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+if (localStorage.getItem('city') == null) {
+	var cityObj = [];
+} else {
+	var cityObj = JSON.parse(localStorage.getItem('city'));
 }
-// var searchBtn = document.querySelector('#search');
-function callFetch(city) {
-	console.log('yo');
+// var historyButtonsEl = document.querySelector('#city-his-btn');
+
+var formSubmitHandler = function (event) {
+	event.preventDefault();
+
+	var city = cityInput.value.trim();
+	console.log(city);
+
+	if (city) {
+		getCityWeather(city);
+	} else {
+		alert('Please enter a city name');
+	}
+};
+
+var buttonClickHandler = function (event) {
+	//TODO:  var city = event.target.getAttribute('data-language');
+	// if (city) {
+	// 	getCityWeather(city);
+	// } else {
+	// 	alert('Please enter a city name');
+	// }
+};
+
+var getCityWeather = async function (city) {
 	var queryURL =
 		'http://api.openweathermap.org/data/2.5/weather?q=' +
 		city +
 		'&appid=' +
 		apiKey;
-	fetch(queryURL, {
+	var displayCity = function (city) {
+		if (city.length === 0) {
+			resultText.textContent = '';
+			resultContentEl.textContent = 'Nothing found, enter a City name.';
+			return;
+		}
+		resultText.textContent = city.name;
+		updateCityObj = {
+			name: city.name,
+			lat: city.coord.lat,
+			lon: city.coord.lon,
+		};
+		console.log(updateCityObj);
+		cityObj.push(updateCityObj);
+		localStorage.setItem('city', JSON.stringify(cityObj));
+	};
+	await fetch(queryURL, {
 		// The browser fetches the resource from the remote server without first looking in the cache.
 		// The browser will then update the cache with the downloaded resource.
 		cache: 'reload',
 	})
 		.then(function (response) {
-			return response.json();
+			if (response.ok) {
+				return response.json();
+			} else {
+				alert('Error: ' + response.statusText);
+			}
 		})
 		.then(function (data) {
 			console.log(data);
-			cityObj = {
-				name: data.name,
-				lat: data.coord.lat,
-				lon: data.coord.lon,
-			};
-			localStorage.setItem('city', JSON.stringify(cityObj));
+			displayCity(data);
 		});
+	getFiveDayForecast(city);
+};
+var getFiveDayForecast = function (city) {
+	console.log(city);
 	var cityHistory = JSON.parse(localStorage.getItem('city'));
-	var lat = cityHistory.lat;
-	var lon = cityHistory.lon;
+	console.log(cityHistory);
+	cityHistoryData = cityHistory.filter(function (obj) {
+		return obj.name == city;
+	});
+	console.log(cityHistoryData);
+	var lat = cityHistoryData[0].lat;
+	var lon = cityHistoryData[0].lon;
 	var fiveDayqueryURL =
 		'https://api.openweathermap.org/data/2.5/forecast?lat=' +
 		lat +
@@ -40,10 +90,7 @@ function callFetch(city) {
 		lon +
 		'&appid=' +
 		apiKey;
-
 	fetch(fiveDayqueryURL, {
-		// The browser fetches the resource from the remote server without first looking in the cache.
-		// The browser will then update the cache with the downloaded resource.
 		cache: 'reload',
 	})
 		.then(function (response) {
@@ -52,4 +99,7 @@ function callFetch(city) {
 		.then(function (data) {
 			console.log(data);
 		});
-}
+};
+
+searchFormEl.addEventListener('submit', formSubmitHandler);
+// historyButtonsEl.addEventListener('click', buttonClickHandler);
